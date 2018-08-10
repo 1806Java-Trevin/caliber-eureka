e57797157eb720948dac46a4445cf27dd08521d7
Kea <KeaUehara@gmail.com>
2018-08-10 15:18:33 -0400

    Removed testing of main method

--- src/test/java/com/revature/caliber/eureka/MainTest.java
+++ src/test/java/com/revature/caliber/eureka/MainTest.java
@@ -18,17 +18,6 @@ static EurekaDiscoveryApplication app;
 		assertEquals("This is a passing test", "Test", app.getTest());
 	}
 	
-	@Test
-	public void testMain() {
-		// assert("Testing main", void, app.main(new String[] {"Test"}));
-		try {
-			app.main(new String[] {"Test"});
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
 	@AfterClass
 	public static void deleteSpring() {
 		app = null;
